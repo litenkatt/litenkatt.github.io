7d8d39f781cd8dcf512fcaca407c5a81a5a7ae29
litenkatt <litenkatt@users.noreply.github.com>
2020-08-27 20:33:32 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.3bd032508d94b067ca50498fae85609c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8d11083c4b88aba9b5488d8a214057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0d288706a2020d76f6",
+    "url": "/static/css/main.5084b4ec.chunk.css"
+  },
+  {
+    "revision": "92750c896dabbf7165de",
+    "url": "/static/js/2.b4fc1955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4fc1955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0d288706a2020d76f6",
+    "url": "/static/js/main.165f6d7e.chunk.js"
+  },
+  {
+    "revision": "70dc1a841f3a220ad405",
+    "url": "/static/js/runtime-main.a004bc19.js"
+  }
+]);
